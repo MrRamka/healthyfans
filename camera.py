d578f58952fa08c1c7b55176ab2e888a63586d67
Ramil Minyukov <rrminyukov@avito.ru>
2022-10-12 01:26:59 +0300

    Detect human and faces

--- camera.py
+++ camera.py
@@ -1,7 +1,11 @@
 import cv2 as cv
+import numpy as np
 
 CLOSE_KEY = 'q'
 USER_CAMERA = 0
+FACE_PATH = 'haarcascade/haarcascade_frontalface_default.xml'
+FULL_BODY_PATH = 'haarcascade/haarcascade_fullbody.xml'
+UPPER_BODY_PATH = 'haarcascade/haarcascade_upperbody.xml'
 
 
 class Camera:
@@ -11,6 +15,9 @@ class Camera:
         if not self.capture.isOpened():
             raise PermissionError('Cant open camera')
         self.running = True
+        self.face_cascade = cv.CascadeClassifier(FACE_PATH)
+        self.hog = cv.HOGDescriptor()
+        self.hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
     def run(self):
         print('Exit - "q"')
@@ -22,11 +29,16 @@ class Camera:
                 if not ret:
                     print("TODO: raise error")
                     break
+
+                # self.draw_rectangle(frame)
+
+                self.draw_body_rectangle(frame)
+
                 # Display the resulting frame
                 cv.imshow(self.frame_title, frame)
 
-                # fix this
-                if cv.waitKey(1) == ord(CLOSE_KEY):
+                # fix this - exit
+                if cv.waitKey(1) & 0xFF == ord(CLOSE_KEY):
                     self.stop()
         except OSError:
             self.stop()
@@ -35,3 +47,20 @@ class Camera:
         self.capture.release()
         cv.destroyAllWindows()
         self.running = False
+
+    def draw_rectangle(self, frame):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    def draw_body_rectangle(self, frame):
+        boxes, weights = self.hog.detectMultiScale(frame)
+
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
